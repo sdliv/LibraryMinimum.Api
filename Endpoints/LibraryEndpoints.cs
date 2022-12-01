1d7d4ee6247deabb3ea76c78aae34b4dfc38a538
sdliv <sean.d.livingston@gmail.com>
2022-12-01 18:37:52 -0500

    Moves route functionality into iheir own internal methods

--- Endpoints/LibraryEndpoints.cs
+++ Endpoints/LibraryEndpoints.cs
@@ -4,6 +4,8 @@ using LibraryMinimumAPI.Endpoints.Internal;
 using LibraryMinimumAPI.Models;
 using LibraryMinimumAPI.Validators;
 using Microsoft.AspNetCore.Cors;
+using System.Reflection;
+using System.Net.NetworkInformation;
 
 namespace LibraryMinimumAPI.Endpoints
 {
@@ -17,87 +19,100 @@ namespace LibraryMinimumAPI.Endpoints
 
         public static void DefineEndpoints(IEndpointRouteBuilder app)
         {
-            app.MapPost("books", async (Book book, IBookService bookService, IValidator<Book> validator, LinkGenerator linker, HttpContext context) =>
+            app.MapPost("books", CreateBookAsync)
+                .WithName("CreateBook")
+                .Accepts<Book>("application/json")
+                .Produces<Book>(201)
+                .Produces<IEnumerable<ValidationFailure>>(400)
+                .WithTags("Books");
+
+            app.MapPut("books/{isbn}", UpdateBookByIsbnAsync) 
+                .WithName("UpdateBook")
+                .Accepts<Book>("application/json")
+                .Produces<Book>(200)
+                .Produces<IEnumerable<ValidationFailure>>(400)
+                .WithTags("Books");
+
+            app.MapGet("books", GetAllBooksAsync)
+                .WithName("GetBooks")
+                .Produces<IEnumerable<Book>>(200)
+                .WithTags("Books");
+
+            app.MapGet("books/{isbn}", GetBookByIsbnAsync)
+                .WithName("GetBook")
+                .Produces<Book>(200)
+                .Produces(404)
+                .WithTags("Books");
+
+            app.MapDelete("books/{isbn}", DeleteBookByIsbn)
+                .WithName("DeleteBook")
+                .Produces(204)
+                .Produces(404)
+                .WithTags("Books");
+
+            app.MapGet("status", StatusByAsync )
+                .ExcludeFromDescription(); // Removes endpoint from swagger. // .RequireCors("AnyOrigin") // Fluid approach to adding Cors.
+        }
+
+        internal static async Task<IResult> CreateBookAsync(Book book, IBookService bookService, IValidator<Book> validator)
+        {
+            var validationResult = await validator.ValidateAsync(book);
+            if (!validationResult.IsValid)
             {
+                return Results.BadRequest(validationResult.Errors);
+            }
 
-                var validationResult = await validator.ValidateAsync(book);
-                if (!validationResult.IsValid)
-                {
-                    return Results.BadRequest(validationResult.Errors);
-                }
-
-                var created = await bookService.CreateAsync(book);
-                if (!created)
-                {
-                    return Results.BadRequest(new List<ValidationFailure>
+            var created = await bookService.CreateAsync(book);
+            if (!created)
+            {
+                return Results.BadRequest(new List<ValidationFailure>
         {
             new ValidationFailure("Isbn", "A book with this ISBN-13 already exists")
-        });
-                }
-                var path = linker.GetPathByName("GetBook", new { isbn = book.Isbn })!;
-                var locationUri = linker.GetUriByName(context, "GetBook", new { isbn = book.Isbn })!;
-                return Results.Created(locationUri, book);
-                // return Results.Created(path, book);
-                // return Results.CreatedAtRoute("GetBook", new { isbn = book.Isbn }, book);
-                // return Results.Created($"/books/{book.Isbn}", book);
-            }).WithName("CreateBook")
-            .Accepts<Book>("application/json")
-            .Produces<Book>(201)
-            .Produces<IEnumerable<ValidationFailure>>(400)
-            .WithTags("Books");
+                });
+            }
+            return Results.Created($"/book/{book.Isbn}", book); 
+        }
 
-            app.MapPut("books/{isbn}", async (Book book, IBookService bookService, IValidator<Book> validator) =>
+        internal static async Task<IResult> GetAllBooksAsync(IBookService bookService, string? searchTerm) 
+        {
+            if (searchTerm is not null && !string.IsNullOrWhiteSpace(searchTerm))
             {
+                var matchedBooks = await bookService.SearchByTitleAsync(searchTerm);
+                return Results.Ok(matchedBooks);
+            }
+            var books = await bookService.GetAllAsync();
+            return Results.Ok(books);
+        }
 
-                var validationResult = await validator.ValidateAsync(book);
-                if (!validationResult.IsValid)
-                {
-                    return Results.BadRequest(validationResult.Errors);
-                }
+        internal static async Task<IResult>GetBookByIsbnAsync(string isbn, IBookService bookService)
+        {
+            var book = await bookService.GetByIsbnAsync(isbn);
 
-                var updated = await bookService.UpdateAsync(book);
-                return updated ? Results.Ok(book) : Results.NotFound();
-            }).WithName("UpdateBook")
-            .Accepts<Book>("application/json")
-            .Produces<Book>(200)
-            .Produces<IEnumerable<ValidationFailure>>(400)
-            .WithTags("Books");
+            return book is not null ? Results.Ok(book) : Results.NotFound();
+        }
 
-            app.MapGet("books", async (IBookService bookService, string? searchTerm) =>
+        internal static async Task<IResult>UpdateBookByIsbnAsync(Book book, IBookService bookService, IValidator<Book> validator)
+        {
+            var validationResult = await validator.ValidateAsync(book);
+            if (!validationResult.IsValid)
             {
-                if (searchTerm is not null && !string.IsNullOrWhiteSpace(searchTerm))
-                {
-                    var matchedBooks = await bookService.SearchByTitleAsync(searchTerm);
-                    return Results.Ok(matchedBooks);
-                }
-                var books = await bookService.GetAllAsync();
-                return Results.Ok(books);
-            }).WithName("GetBooks")
-            .Produces<IEnumerable<Book>>(200)
-            .WithTags("Books");
+                return Results.BadRequest(validationResult.Errors);
+            }
 
-            app.MapGet("books/{isbn}", async (string isbn, IBookService bookService) =>
-            {
-                var book = await bookService.GetByIsbnAsync(isbn);
+            var updated = await bookService.UpdateAsync(book);
+            return updated ? Results.Ok(book) : Results.NotFound();
+        }
 
-                return book is not null ? Results.Ok(book) : Results.NotFound();
-            }).WithName("GetBook")
-            .Produces<Book>(200)
-            .Produces(404)
-            .WithTags("Books");
+        internal static async Task<IResult>DeleteBookByIsbn(string isbn, IBookService bookService)
+        {
+            var deleted = await bookService.DeleteAsync(isbn);
+            return deleted ? Results.NoContent() : Results.NotFound();
+        }
 
-            app.MapDelete("books/{isbn}", async (string isbn, IBookService bookService) =>
-            {
-                var deleted = await bookService.DeleteAsync(isbn);
-                return deleted ? Results.NoContent() : Results.NotFound();
-            }).WithName("DeleteBook")
-            .Produces(204)
-            .Produces(404)
-            .WithTags("Books");
-
-            app.MapGet("status", [EnableCors("AnyOrigin")] () =>
-            {
-                return Results.Extensions.Html(@"<!doctype html>
+        [EnableCors("AnyOrigin")]
+        internal static async Task<IResult>StatusByAsync()
+        {
+            return Results.Extensions.Html(@"<!doctype html>
             <html>
                 <head><title>Status Page</title></head>
              <body>
@@ -105,7 +120,6 @@ namespace LibraryMinimumAPI.Endpoints
                 <p>The server is working fine. Bye bye!</p>
             </body>
             </html>");
-            }).ExcludeFromDescription(); // Removes endpoint from swagger. // .RequireCors("AnyOrigin") // Fluid approach to adding Cors.
         }
     }
 }
